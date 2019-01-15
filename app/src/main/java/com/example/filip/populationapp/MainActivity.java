5eeb3fca71d03a9bbb9b56ad66318f18d971b253
Your Name <Filip>
2019-01-15 17:57:26 +0100

    First commit

---
+++ app/src/main/java/com/example/filip/populationapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.filip.populationapp;
+
+import android.app.VoiceInteractor;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AsyncTask asyncTask = new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        OkHttpClient client = new OkHttpClient();
+                        Request request = new Request.Builder()
+                                .url("http://api.population.io:80/1.0/population/1980/Brazil/18/")
+                                .build();
+                        Response response = null;
+
+                        try{
+                            response = client.newCall(request).execute();
+                            return response.body().string();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Object o) {
+                        textView.setText(o.toString());
+                    }
+                }.execute();
+            }
+        });
+
+
+    }
+}
